9d606e7d85ce30f079ef5bd3b7381b62631bc1e7
romainleprivey35-hash <romain.leprivey35@gmail.com>
2026-02-15 14:42:24 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    SHEET_ID: '1Vw439F_75oc7AcxkDriWi_fwX2oBbAejnp-f_Puw-FU',
+    NINTENDO_LOGO: '1UjAmaGcJuACX5jIkaDu6OdHA4fkKokp-',
+    CONSOLE_LOGOS: {
+        "PS1": "1DV-N37sM1AA-fl5rYe1_Urr6hh9e6eTF",
+        "PS2": "10h2eIupplXfFBvQQNWRptu1EC3xTkghc",
+        "PS3": "1eqVPNUIqNwzdPs4j6ALB922qNvSJozFA",
+        "PS4": "1VjijUcf3nyaclZazZT8akg_4Ifo0UjGM",
+        "PS5": "1F_qvq4AM8uvx1nKaRUdPWh5r1mjVqic8",
+        "N64": "1iumJt5i-5Jd85ZPZLr3NR44hbhdohUh5",
+        "GameCube": "1SW-jXEJnlZ4nh3jXg3IVkNGK7QzZMSze",
+        "Wii": "1aAx82c4LPWz6U-JQ2jXf3fG0hwAOG_Bc",
+        "GBA": "11vgmA2xIMxNHbbYMcUIwXTNgt0BVKacp",
+        "GB": "1XEkPuCr2mmIvpsmjmpkrG1XumS-24wLb",
+        "GBC": "1dek_9N4wDwFBhSYmUoij7OhtFfCc4hcQ",
+        "DS": "1Gals-7-g_lNxOBult4FihHYiv2nKgkfP",
+        "Xbox": "15i7MRlq_QVyKUsQKWsFfMSkvqALFwb2I",
+        "PSP": "1zOJp5Yh0JRHhI-o4PfyFTdX_OH7j3Bmo"
+    }
+};
