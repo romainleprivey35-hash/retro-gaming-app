424e79d61930d81c00484fd96febc36a70363808
romainleprivey35-hash <romain.leprivey35@gmail.com>
2026-02-16 14:08:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,8 @@
 const CONFIG = {
-    // Ton ID de tableau Google Sheets
     SHEET_ID: '1Vw439F_75oc7AcxkDriWi_fwX2oBbAejnp-f_Puw-FU',
-    
-    // Les noms EXACTS de tes onglets (attention à la majuscule)
     TABS: {
-    JEUX: 'Jeux',
-    CONSOLES: 'Consoles',
-    ACCESSOIRES: 'Accessoires' // <-- Vérifie que le nom est identique à ton onglet Sheets
-}
-
-    // Les logos qui s'affichent en haut des listes de jeux
-    CONSOLE_LOGOS: {
-        "PS1": "1DV-N37sM1AA-fl5rYe1_Urr6hh9e6eTF",
-        "PS2": "10h2eIupplXfFBvQQNWRptu1EC3xTkghc",
-        "PS3": "1eqVPNUIqNwzdPs4j6ALB922qNvSJozFA",
-        "PS4": "1VjijUcf3nyaclZazZT8akg_4Ifo0UjGM",
-        "PS5": "1F_qvq4AM8uvx1nKaRUdPWh5r1mjVqic8",
-        "N64": "1iumJt5i-5Jd85ZPZLr3NR44hbhdohUh5",
-        "Xbox": "15i7MRlq_QVyKUsQKWsFfMSkvqALFwb2I"
+        JEUX: 'Jeux',
+        CONSOLES: 'Consoles',
+        ACCESSOIRES: 'Accessoires'
     }
 };
